a3d909cc1c952ede5135f4ceec1b8209fb1fc967
devXcant <devxcant@gmail.com>
2024-06-05 09:41:56 +0100

    connect db

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(
+  PROCESS.ENV.MONGO
+).then(
+    ()=> {console.log('mongodb is connected')}
+).catch((err)=> {
+    console.log(err)
+});
+
+
+const app = express();
+
+app.listen(3000, ()=> {
+    console.log('Server is running on port 3000')
+})
