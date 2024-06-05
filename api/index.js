aae35428054f72dee5c48192a99f911afe544796
devXcant <devxcant@gmail.com>
2024-06-05 10:51:32 +0100

    signup backend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
 mongoose.connect(
-  PROCESS.ENV.MONGO
+    process.env.MONGO
 ).then(
     ()=> {console.log('mongodb is connected')}
 ).catch((err)=> {
@@ -14,7 +16,23 @@ mongoose.connect(
 
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000')
+})
+
+
+app.use('/api/user', userRoutes )
+app.use('/api/auth', authRoutes)
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
