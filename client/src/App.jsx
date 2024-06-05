79fc5d977329ab95c20cc99cd8c3768ac7566dd9
devXcant <devxcant@gmail.com>
2024-06-05 07:46:43 +0100

    fronted pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className='text-3xl'> App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/profile" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
