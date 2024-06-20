3e99700461b9b1afdd6928fe6541c38efb6f8c46
devXcant <devxcant@gmail.com>
2024-06-20 10:16:36 +0100

    redux store setup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,21 +34,22 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password || !email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
   }
 
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      next(errorHandler(404, "User not found"));
+      return next(errorHandler(404, "User not found"));
     }
-    const validPasssword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPasssword) {
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
       return next(errorHandler(400, "Invalid Password"));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.envv.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
